0bc6eb88644f1df9fa125bd4100f1d563f45f6e8
imshaz khan <imshaz.khan@elktech.co>
2022-09-07 20:49:24 +0500

    mask objec and array fields

---
+++ mask-object-array.js
@@ -0,0 +1,55 @@
+const myObject = {
+  propA: "a",
+  propB: { propBA: "ba", propBB: "bb" },
+  propC: ["c0", "c1"],
+  propD: {
+    propDA: "da",
+    propDB: { propDBA: "dba", propDBB: "dbb" },
+    propDC: ["dc0", "dc1"],
+  },
+  propE: ["e0", { propE1A: "e1a", propE1B: "e1b" }, ["e20", "e21"]],
+};
+
+function isArray(data) {
+  return Array.isArray(data);
+}
+
+function isPlainObject(data) {
+  return typeof data === "object" && data !== null && !Array.isArray(data);
+}
+
+const maskedFieldsKeys = ["propA", "propDBA", "propE1B", "propD", "propC", "0"];
+const mask = (objOrArr) => {
+  const masked = {};
+  for (let key in objOrArr) {
+    const val = objOrArr[key];
+    if (isPlainObject(val) || isArray(val)) {
+      masked[key] = mask(val);
+    } else {
+      let maskedValue = "";
+
+      console.log("=======");
+      console.log({
+        key,
+        maskedField: objOrArr[key],
+        include: maskedFieldsKeys.includes(key),
+      });
+      console.log("=======");
+
+      masked[key] = objOrArr[key];
+      if (maskedFieldsKeys.includes(key)) {
+        [...objOrArr[key]].forEach((item) => {
+          // console.log("========", item);
+          maskedValue = maskedValue + "*";
+          //   masked[key] = "*****";
+        });
+        masked[key] = maskedValue;
+        // return masked;
+      }
+    }
+  }
+  return masked;
+};
+
+console.log("original... ", myObject);
+console.log("masked... ", mask(myObject));
